0aace5f901b1120a1d6989317e7463b0683f56b6
slom64 <slomkm5@gmail.com>
2024-11-21 11:04:38 +0200

    Warraky_semiStart

--- MLP.py
+++ MLP.py
@@ -6,16 +6,25 @@ CLASSES = 3
 def mlp(data,hidden_layers, eta, epochs, bias_flag, sigmoid0_tangent1):
 
     layers = []
-
+    weights = []  # --> should contatin the bias also if bias_flag true
     for epoch in range(epochs):
         for row in data:
-            activation_output = forward(row, wights, bias,bias_flag, sigmoid0_tangent1)
-            wights,bias=backward(row, activation_output, wights, bias, bias_flag, eta, sigmoid0_tangent1)
-    return wights,bias
+            activation_outputs = forward(row, weights,sigmoid0_tangent1)
+            weights=backward(row, activation_outputs, weights, sigmoid0_tangent1)
+            weights_update(row, activation_outputs, weights, eta)
+    return weights
 
-def forward(row, wights, bias,bias_flag, sigmoid0_tangent1):
+def forward(input_layer, weights, bias,bias_flag, sigmoid0_tangent1):
     return
 
-def backward(row, activation_output, wights, bias, bias_flag, eta, sigmoid0_tangent1):
-    
+def backward(input_layer, activation_output, weights, bias, bias_flag, eta, sigmoid0_tangent1):
+    return
+
+def weights_update(row, activation_outputs, weights, eta):
+    return
+
+def sigmoid():
+    return
+
+def tangent():
     return
