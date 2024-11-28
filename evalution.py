3e6e0468401c8a43349ce9a84cf4367c3ff6fdf1
IT <osamakhaled31415@gmail.com>
2024-11-28 15:34:04 +0200

    y_actual for all x_test

--- evalution.py
+++ evalution.py
@@ -1,20 +1,14 @@
-import numpy as np
 from MLP import forward
 
 
-def y_actual(inputs, weights,bias_flag, sigmoid0_tangent1):
-    neourons = forward(inputs, weights,bias_flag, sigmoid0_tangent1)
-    output_layer = neourons[-1]
-    output_values = output_layer[:3]
-    print(output_values)
-    actual_class = output_values.index(max(output_values))
+def y_actual(x_test, weights,bias_flag, sigmoid0_tangent1):
+    actual_class =[]
+    for test_element in x_test:
+        neurons = forward(test_element, weights,bias_flag, sigmoid0_tangent1)
+        output_layer = neurons[-1]
+        output_values = output_layer[:3]
+        print(output_values)
+        actual_class.append(output_values.index(max(output_values)))
     return actual_class
 
-weights = [
-    [[0.1, 0.2, 0.3, 0.1], [0.2, 0.3, 0.4, 0.2]],  # Layer 1
-    [[0.8, 0.7, 0.6], [0.9, 0.8, 0.7], [0.4, 0.3, 0.1]]  # Output layer
-]
 
-inputs = [0.5, 0.8, 0.2]
-output = y_actual(inputs, weights, 1, 0)
-print(output)
